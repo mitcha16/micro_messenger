2439b86c90c6c27b49c579c58e99c6f6f9357850
mitchashby16 <mitchashby16@gmail.com>
2015-08-18 14:30:40 -0600

    fixed routes paht to exclude static_pages

--- spec/features/visitor_can_view_static_pages_spec.rb
+++ spec/features/visitor_can_view_static_pages_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 feature 'visitor can view static pages' do
   it 'can view the home page' do
-    visit static_pages_home_path
+    visit root_path
 
     expect(page).to have_content('Home')
   end
 
   it 'can view the help page' do
-    visit static_pages_help_path
+    visit help_path
 
     expect(page).to have_content('Help')
   end
 
   it 'can view the about page' do
-    visit static_pages_about_path
+    visit about_path
 
     expect(page).to have_content('About')
   end
