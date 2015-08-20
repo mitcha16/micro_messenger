34565ee64b55b6f01c7350c43d2bdf840299e661
mitchashby16 <mitchashby16@gmail.com>
2015-08-20 14:15:08 -0600

    invalid users cannot sign up

--- spec/features/users_signup_spec.rb
+++ spec/features/users_signup_spec.rb
@@ -13,4 +13,28 @@ feature 'user can sign up' do
     expect(page).to have_content("Jessica")
   end
 
+  it 'cannot sign up a user without a name' do
+    visit signup_path
+    fill_in "user[name]", with: ""
+    fill_in "user[email]", with: "j@gmail.com"
+    fill_in "user[password]", with: "password"
+    fill_in "user[password_confirmation]", with: "password"
+    click_button "Create my account"
+
+    expect(current_path).to eq(users_path)
+    expect(page).to have_content("Name can't be blank")
+  end
+
+  it 'cannot sign up a user without an email' do
+    visit signup_path
+    fill_in "user[name]", with: "jess"
+    fill_in "user[email]", with: ""
+    fill_in "user[password]", with: "password"
+    fill_in "user[password_confirmation]", with: "password"
+    click_button "Create my account"
+
+    expect(current_path).to eq(users_path)
+    expect(page).to have_content("Email can't be blank")
+  end
+
 end
